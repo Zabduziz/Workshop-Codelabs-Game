893ceb7f720c158e3cff5961c89c267cb03db0c2
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2023-01-05 05:47:20 +0700

    first commit

---
+++ Workshop Codelabs/Assets/Scripts/mainmenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+    public void BackGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
